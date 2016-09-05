80db29b291026f677b389f9abdc8d5f56edc9043
grenaria <alex@corallabs.com>
2016-09-05 11:27:57 -0700

    WIP adding nesting functionality, 2 test cases still fail.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # usage-track
 Usage track is a small JS library for tracking web usage via markup.
+
+## Basic tracking example
+
+Place your configuration in a data attribute on an element of the page, and mark elements that you want click tracking.
+
+[BasicTrack.html](examples/BasicTrack.html)
+```html
+<html data-usagetrack-config='{"clickDefault": "ga-event"}'>
+...
+<button data-usagetrack='{"data": ["Category", "Action", "Label", 1234]}'>Click Me</button>
+```
+
--- usage-track.js
+++ usage-track.js
@@ -2,7 +2,10 @@ var usagetrack;
 (function (global, dom, $) {
    var config = {
          clickDefault: null,
-         clickDefaultDom: dom
+         clickDefaultDom: dom,
+         clickDefaultConfig: {
+            clickEvent: true
+         }
       },
       domConfig,
       aHandlers = {};
@@ -25,12 +28,46 @@ var usagetrack;
       ga('send', 'event', aData[0], aData[1], aData[2], aData[3]);
    };
 
+   aHandlers['console'] = function(jEl, aData) {
+      console.log('Usage Click Event:', aData);
+   };
+
    function onClick(event) {
       var jEl = $(event.target),
-        usage = jEl.data('usagetrack');
-      if (typeof usage == 'object') {
+        usage,
+        usageData = [],
+        aParents = jEl.parents().get().reverse();
+      aParents.push(jEl.get(0));
+      $.each(aParents, function() {
+         var jParent = $(this),
+            sGroup = jParent.data('usagetrackGroup'),
+            parentUsage = jParent.data('usagetrack');
+         if (sGroup) {
+            if (typeof sGroup != 'string') {
+               throw "Usage group must be a string";
+            }
+            usageData.push(sGroup);
+         } else if (parentUsage) {
+            if (typeof parentUsage == 'string') {
+               usageData.push(parentUsage);
+               if (!usage) {
+                  usage = $.extend({}, config.clickDefaultConfig);
+               }
+               usage.clickEvent = config.clickDefaultConfig.clickEvent;
+            } else if (typeof parentUsage === 'object') {
+               if (!usage) {
+                  usage = $.extend({}, config.clickDefaultConfig);
+               }
+               $.extend(usage, parentUsage);
+               if (parentUsage.clickEvent !== false) {
+                  usage.clickEvent = config.clickDefaultConfig.clickEvent;
+               }
+            }
+         }
+      });
+      if (usage && usage.clickEvent) {
          if (config.clickDefault) {
-            handle(config.clickDefault, jEl, usage.data);
+            handle(config.clickDefault, jEl, usageData ? (usage.data ? $.merge(usageData, usage.data) : usageData) : usage.data);
          }
       }
    }
